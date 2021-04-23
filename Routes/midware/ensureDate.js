1dbe28f0811044d16ca057f1ececa636ddfb9b50
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-23 03:06:19 +0300

    Ensuring that date of birth is parseable and at least 4 years ago

--- Routes/midware/ensureDate.js
+++ Routes/midware/ensureDate.js
@@ -1,23 +1,41 @@
-const config = require('config')
+const config = require("config")
+
+var isDate = function (date) {
+    return new Date(date) !== "Invalid Date" && !isNaN(new Date(date))
+}
 
 module.exports = function (date) {
-
-    return  function(req, res, next){
+    return function (req, res, next) {
         try {
-
-            if (!token)
-            {
+            if (!dateOfBirth) {
                 return res
-                .status(403)
-                .json({message: "You must enter the bith date!"})
+                    .status(403)
+                    .json({ message: "You must enter the bith date!" })
+            } else {
+                if (!isDate(date)) {
+                    return res
+                        .status(403)
+                        .json({
+                            message:
+                                "Error: can't parse date of birth as date!",
+                        })
+                } else {
+                    let year =
+                        new Date(date).getFullYear() - new Date.getFullYear()
+                    if (year < 4) {
+                        return res
+                            .status(403)
+                            .json({
+                                message:
+                                    "Invalid date of birth!",
+                            })
+                    }
+                }
             }
-    
             next()
         } catch (e) {
             console.log(e)
-            return res
-            .status(403)
-            .json({message: "Error occured!"})
+            return res.status(403).json({ message: "Error occured!" })
         }
     }
 }
