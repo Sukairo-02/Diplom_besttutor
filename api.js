ecd2d70b3b92e0f40d90faacc91f5c9b8daf17d9
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-28 11:03:38 +0300

    New route, syntax fix

--- api.js
+++ api.js
@@ -1,37 +1,39 @@
-const config = require('config');
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-app.use(express.json({ extended: true }));
+const config = require("config")
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+app.use(express.json({ extended: true }))
 
-const authRouter = require('./Routes/auth');
-app.use('/api/auth', authRouter);
+const authRouter = require("./Routes/auth")
+const schoolRouter = require("./Routes/school")
+app.use("/api/auth", authRouter)
+app.use("/api/school", schoolRouter)
 
-const PORT = process.env.PORT || config.get('server.PORT');
+const PORT = process.env.PORT || config.get("server.PORT")
 
 const start = async () => {
-	try {
-		await mongoose.connect(
-			config.get('mongoose.URI'),
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			},
-			(err) => {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log('Database connected succesfully!');
-				}
-			}
-		);
+    try {
+        await mongoose.connect(
+            config.get("mongoose.URI"),
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+            (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Database connected succesfully!")
+                }
+            }
+        )
 
-		app.listen(PORT, () => {
-			console.log(`Server has been started at port: ${PORT}`);
-		});
-	} catch (e) {
-		console.log(e);
-	}
-};
+        app.listen(PORT, () => {
+            console.log(`Server has been started at port: ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-start();
+start()
