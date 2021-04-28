e78809e4f3acb409fd89551d4bdfd1f435023371
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-29 02:53:12 +0300

    New field: Desc

--- Models/Subjects.js
+++ Models/Subjects.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const Subjects = new Schema({
 	name: { type: String, unique: true, required: true },
+	desc: {type: String},
 	Teachers: [{ type: String }],
 })
 
