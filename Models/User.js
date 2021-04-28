80feba7f227273d36ba381a66b739fb70aedea35
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-29 02:18:00 +0300

    Moved phone and city from Teacher, added fields 'city', 'address'

--- Models/User.js
+++ Models/User.js
@@ -5,13 +5,13 @@ const User = new Schema({
 	password: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	dateOfBirth: { type: Date, required: true },
-	avatar: { type: String },
-	roles: [{ type: String, ref: 'Role' }], //possible roles: 'USER', 'TCHR'
-	isActive: { type: Boolean, required: true, default: false },
 	phone: { type: String, default: 'TBA' },
 	area: { type: String, default: 'TBA' },
 	city: { type: String, default: 'TBA' },
 	address: { type: String, default: 'TBA' },
+	avatar: { type: String },
+	roles: [{ type: String, ref: 'Role' }], //possible roles: 'USER', 'TCHR'
+	isActive: { type: Boolean, required: true, default: false },
 })
 
 module.exports = model('User', User)
