655bfec10693d61afaa986c200ed4678df4ece21
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-30 01:15:54 +0300

    Added expiration to model

--- Models/Resetpass.js
+++ Models/Resetpass.js
@@ -2,7 +2,8 @@ const { Schema, model } = require("mongoose")
 
 const Resetpass = new Schema({
     value: { type: String, unique: true, required: true},
-    src: {type: String, required: true}
+    src: {type: String, required: true},
+    createdAt: { type: Date, expires: "15m", default: Date.now }
 })
 
 module.exports = model("Resetpass", Resetpass)
