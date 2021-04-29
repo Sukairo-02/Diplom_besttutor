3dada07a384419fe6a723cc9e831534ade14ea21
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-29 03:16:03 +0300

    New middleware to check if teacher has filled out his data

---
+++ Routes/midware/ensureActiveteacher.js
@@ -0,0 +1,29 @@
+//SHOULD ONLY BE USED AFTER ensureRoles(['TCHR'...])
+const Teacher = require ('../../models/Teacher')
+
+module.exports = function (roles) {
+    return (req, res, next) => {
+        if (req.method === "OPTIONS") {
+            return next()
+        }
+    
+        try {
+            const candidate = await Teacher.findOne({src: req.user.id})
+            if(!candidate){
+                return res.status(403).json({message:"Error: nonexistent teacher!"})
+            }
+
+            if(!Teacher.isActive){
+                return res.status(403).json({message:"Error: you must fill out your teacher data first!"})
+            }
+
+            res.dbTeacher = candidate
+            return next()
+        } catch (e) {
+            console.log(e)
+            return res
+            .status(401)
+            .json({message: "User unauthorized!"})
+        }
+    }
+}
