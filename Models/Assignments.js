08e52872e570a1e26f78545fecc5845e42a5ca13
Sukairo-02 <reka.serzh@yandex.ru>
2021-04-30 03:57:25 +0300

    Added model for assignments (tests)

---
+++ Models/Assignments.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose')
+
+const Assignments = new Schema({
+	title: { type: String, required: true },
+	desc: { type: String },
+	isShuffled: { type: Boolean, required: true, default: false }, //determines if questions and answer variants will be shuffled
+	allowOvertime: { type: Boolean, required: true, default: false }, //determines if users will be able to submit past endDate
+	maxPoints: { type: Number, default: 0 },
+	date: { type: Date, required: true, default: Date.now },
+	endDate: { type: Date },
+	questions: [
+		{
+			title: { type: String, required: true },
+			qID: Schema.Types.ObjectId,
+			points: { type: Number, default: 0 },
+			answers: [
+				{
+					nID: Schema.Types.ObjectId,
+					text: { type: String, required: true },
+					isTrue: { type: Boolean, required: true, default: false },
+				},
+			],
+		},
+	],
+	answers: [
+		{
+			submitter: { type: String, required: true },
+			qID: { type: String, required: true },
+			answers: [
+				{
+					nID: { type: String, required: true },
+					isChecked: {
+						type: Boolean,
+						required: true,
+						default: false,
+					},
+				},
+			],
+		},
+	],
+})
+
+module.exports = model('Assignments', Assignments)
