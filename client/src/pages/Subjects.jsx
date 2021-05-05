4cd5d48e32f86f007e76a6a0b8b3ede63bd19d3e
Kybbot <kybbot1@gmail.com>
2021-05-05 22:41:02 +0300

    updated Subjects page

--- client/src/pages/Subjects.jsx
+++ client/src/pages/Subjects.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSubjects } from '../redux/reducers/subjectsSlice';
+
+const colors = [
+	'rgba(229, 143, 143, 0.5)',
+	'rgba(91, 255, 196, 0.5)',
+	'rgba(129, 126, 255, 0.5)',
+	'rgba(219, 219, 219, 0.5)',
+	'rgba(227, 143, 229, 0.5)',
+	'rgba(237, 107, 185, 0.5)',
+	'rgba(255, 245, 0, 0.5)',
+	'rgba(143, 223, 229, 0.5)',
+	'rgba(255, 184, 144, 0.5)',
+	'rgba(255, 0, 229, 0.5)',
+	'rgba(112, 255, 0, 0.5)',
+	'rgba(0, 240, 255, 0.5)',
+	'rgba(0, 71, 255, 0.5)',
+	'rgba(255, 184, 0, 0.5)',
+];
 
 const Subjects = () => {
+	const dispatch = useDispatch();
+	const subjects = useSelector((state) => state.subjects.items);
+
+	React.useEffect(() => {
+		if (!subjects.length) {
+			dispatch(fetchSubjects());
+		}
+	});
+
 	return (
 		<main className='main'>
 			<div className='container'>
 				<div className='subjects-list'>
-					<div className='subjects-list__item'>Английский</div>
-					<div className='subjects-list__item'>Программирование</div>
-					<div className='subjects-list__item'>Химия</div>
-					<div className='subjects-list__item'>История</div>
-					<div className='subjects-list__item'>Физика</div>
-					<div className='subjects-list__item'>Пение</div>
-					<div className='subjects-list__item'>Рисование</div>
-					<div className='subjects-list__item'>Право</div>
-					<div className='subjects-list__item'>Литература</div>
-					<div className='subjects-list__item'>Японский</div>
-					<div className='subjects-list__item'>Экономика</div>
-					<div className='subjects-list__item'>Украинский</div>
-					<div className='subjects-list__item'>Геометрия</div>
-					<div className='subjects-list__item'>Математика</div>
-					<div className='subjects-list__item'>Астраномия</div>
-					<div className='subjects-list__item'>Биология</div>
+					{subjects &&
+						subjects.map((subject) => (
+							<div
+								style={{
+									backgroundColor:
+										colors[Math.floor(Math.random() * colors.length)],
+								}}
+								key={subject._id}
+								className='subjects-list__item'>
+								{subject.name}
+							</div>
+						))}
 				</div>
 			</div>
 		</main>
