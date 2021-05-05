731d91676adfb9044422507aaa616778f3345e3a
Kybbot <kybbot1@gmail.com>
2021-05-05 22:40:27 +0300

    updated Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserInfo } from '../redux/reducers/userInfoSlice';
+import { TeacherProfile, UserProfile } from '../Components';
 
 const Home = () => {
+	const dispatch = useDispatch();
+	const info = useSelector((state) => state.userInfo.info);
+
+	React.useEffect(() => {
+		if (Object.keys(info).length === 0) {
+			dispatch(fetchUserInfo());
+		}
+	});
+
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<main className='main'>
+			<div className='container'>
+				<div className='content'>
+					{Object.keys(info).length !== 0 && (
+						<>
+							{info.roles[0] === 'TCHR' ? <TeacherProfile /> : <UserProfile />}
+						</>
+					)}
+				</div>
+			</div>
+		</main>
 	);
 };
 
