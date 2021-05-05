65e1e0b5ed9e59a19964866a93f45c25c9b1b580
Kybbot <kybbot1@gmail.com>
2021-05-05 22:37:02 +0300

    updated index.js for Components

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,5 +1,13 @@
 export { default as Header } from './Header';
 export { default as Schedule } from './Schedule';
 export { default as TeacherBig } from './TeacherBig';
+
+export { default as TeacherForm } from './TeacherForm';
 export { default as FormInput } from './FormInput';
 export { default as FormSelect } from './FormSelect';
+export { default as FormTextarea } from './FormTextarea';
+export { default as FormFile } from './FormFile';
+export { default as EditForm } from './EditForm';
+
+export { default as TeacherProfile } from './TeacherProfile';
+export { default as UserProfile } from './UserProfile';
