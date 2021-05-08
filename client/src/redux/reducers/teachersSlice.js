1012a9dec1af1481deaaa52397a5d8b00a7729fb
Kybbot <kybbot1@gmail.com>
2021-05-08 21:52:23 +0300

    updated reducers

--- client/src/redux/reducers/teachersSlice.js
+++ client/src/redux/reducers/teachersSlice.js
@@ -11,14 +11,64 @@ export const fetchTeachers = createAsyncThunk(
 	}
 );
 
+export const fetchTeacherCourses = createAsyncThunk(
+	'teachers/fetchTeacherCourses',
+	async (id) => {
+		const response = await authFetch(`/api/school/courses/${id}`);
+		return response.json();
+	}
+);
+
+export const fetchTeacherReviews = createAsyncThunk(
+	'teachers/fetchTeacherReviews',
+	async (_, { getState }) => {
+		const {
+			teacher: { reviews },
+		} = getState().teachers;
+		const usersIds = reviews.map((review) => review.author);
+
+		const response = await authFetch('/api/auth/lightdataArr/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ ids: usersIds }),
+		});
+		let data = await response.json();
+
+		const newReviews = [];
+
+		await reviews.forEach((review) => {
+			data.users.forEach((user) => {
+				if (review.author === user._id) {
+					newReviews.push(
+						(review = {
+							...review,
+							...user,
+						})
+					);
+				}
+			});
+		});
+
+		return newReviews;
+	}
+);
+
 export const teachersSlice = createSlice({
 	name: 'teachers',
 	initialState: {
 		items: [],
+		teacher: {},
 		loading: 'idle',
 		error: null,
 	},
-	reducers: {},
+	reducers: {
+		setTeacher: (state, action) => {
+			const teacher = state.items.find((item) => item._id === action.payload);
+			state.teacher = teacher;
+		},
+	},
 	extraReducers: {
 		[fetchTeachers.pending]: (state) => {
 			if (state.loading === 'idle') {
@@ -37,7 +87,45 @@ export const teachersSlice = createSlice({
 				state.error = action.error;
 			}
 		},
+
+		[fetchTeacherCourses.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchTeacherCourses.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.teacher.teacherCourses = action.payload.courses;
+			}
+		},
+		[fetchTeacherCourses.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+
+		[fetchTeacherReviews.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchTeacherReviews.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.teacher.reviews = action.payload;
+			}
+		},
+		[fetchTeacherReviews.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
 	},
 });
+
+export const { setTeacher } = teachersSlice.actions;
 
 export default teachersSlice.reducer;
--- client/src/redux/reducers/userInfoSlice.js
+++ client/src/redux/reducers/userInfoSlice.js
@@ -7,6 +7,33 @@ export const fetchUserInfo = createAsyncThunk(
 	'userInfo/fetchUserInfo',
 	async () => {
 		const response = await authFetch('/api/auth/userdata');
+		const data = await response.json();
+
+		if (data.courses.length) {
+			const userCourses = await authFetch('/api/school/usercourses/');
+			const userCoursesData = await userCourses.json();
+
+			data.courses = userCoursesData.courses;
+
+			return data;
+		}
+
+		return data;
+	}
+);
+
+export const fetchTeacherCourses = createAsyncThunk(
+	'userInfo/fetchTeacherCourses',
+	async () => {
+		const response = await authFetch('/api/school/courses/');
+		return response.json();
+	}
+);
+
+export const fetchUserCourses = createAsyncThunk(
+	'userInfo/fetchUserCourses',
+	async () => {
+		const response = await authFetch('/api/school/usercourses/');
 		return response.json();
 	}
 );
@@ -32,6 +59,42 @@ export const userInfoSlice = createSlice({
 			}
 		},
 		[fetchUserInfo.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+
+		[fetchTeacherCourses.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchTeacherCourses.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.info.teacherCourses = action.payload.courses;
+			}
+		},
+		[fetchTeacherCourses.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+
+		[fetchUserCourses.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchUserCourses.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.info.courses = action.payload.courses;
+			}
+		},
+		[fetchUserCourses.rejected]: (state, action) => {
 			if (state.loading === 'pending') {
 				state.loading = 'idle';
 				state.error = action.error;
