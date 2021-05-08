7235cc612ce7419a018a38207c8a95702c0d1889
Kybbot <kybbot1@gmail.com>
2021-05-08 21:49:12 +0300

    updated location for icons

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Star } from './Star';
+export { default as People } from './People';
+export { default as Edit } from './Edit';
+export { default as ChevronDown } from './ChevronDown';
