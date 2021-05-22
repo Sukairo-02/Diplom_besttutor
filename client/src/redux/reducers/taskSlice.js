69bed0208b3d4fee85655b3e119c45eff15eb642
Kybbot <kybbot1@gmail.com>
2021-05-22 21:54:54 +0300

    added reducer for work with assignment datas

---
+++ client/src/redux/reducers/taskSlice.js
@@ -0,0 +1,109 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAuthProvider } from '../../jwt';
+
+const { authFetch } = createAuthProvider();
+
+export const fetchTaskForTeacher = createAsyncThunk(
+	'task/fetchTaskForTeacher',
+	async (id) => {
+		const data = await authFetch('/api/school/getassignment_teacher', 'POST', {
+			assignmentID: id,
+		});
+		return data;
+	}
+);
+
+export const fetchStatisticForTeacher = createAsyncThunk(
+	'task/fetchStatisticForTeacher',
+	async (id) => {
+		const data = await authFetch('/api/school/getstatistic', 'POST', {
+			assignmentID: id,
+		});
+		return data;
+	}
+);
+
+export const fetchTaskForStudent = createAsyncThunk(
+	'task/fetchTaskForStudent',
+	async (id) => {
+		const data = await authFetch('/api/school/getassignment', 'POST', {
+			assignmentID: id,
+		});
+
+		data.questions.forEach((question) => {
+			question.answers.forEach((answer) => {
+				answer.isChecked = false;
+			});
+		});
+
+		return data;
+	}
+);
+
+export const taskSlice = createSlice({
+	name: 'task',
+	initialState: {
+		item: {},
+		statistic: {},
+		loading: 'idle',
+		error: null,
+	},
+	reducers: {},
+	extraReducers: {
+		[fetchTaskForTeacher.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchTaskForTeacher.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.item = action.payload.assignment;
+			}
+		},
+		[fetchTaskForTeacher.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+
+		[fetchStatisticForTeacher.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchStatisticForTeacher.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.statistic = action.payload.statistics;
+			}
+		},
+		[fetchStatisticForTeacher.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+
+		[fetchTaskForStudent.pending]: (state) => {
+			if (state.loading === 'idle') {
+				state.loading = 'pending';
+			}
+		},
+		[fetchTaskForStudent.fulfilled]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.item = action.payload;
+			}
+		},
+		[fetchTaskForStudent.rejected]: (state, action) => {
+			if (state.loading === 'pending') {
+				state.loading = 'idle';
+				state.error = action.error;
+			}
+		},
+	},
+});
+
+export default taskSlice.reducer;
