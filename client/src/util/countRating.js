77b1f35a41df7a5407267c4298a9616f2cf0a3b3
Kybbot <kybbot1@gmail.com>
2021-05-22 21:52:13 +0300

    added Math.round

--- client/src/util/countRating.js
+++ client/src/util/countRating.js
@@ -2,7 +2,7 @@ const countRating = (arr) => {
 	const value = arr.reduce((prev, review) => prev + review.rating, 0);
 	if (value === 0) return 0;
 
-	return value / arr.length;
+	return Math.round(value / arr.length);
 };
 
 export default countRating;
