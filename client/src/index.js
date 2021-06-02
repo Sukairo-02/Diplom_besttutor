b26bb0fa8dfe799b2f75ffdfa4da9420eac47265
Kybbot <kybbot1@gmail.com>
2021-06-02 15:29:57 +0300

    added strictMode

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,10 @@ import { Provider } from 'react-redux';
 import './assets/css/index.css';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
 	document.getElementById('root')
 );
