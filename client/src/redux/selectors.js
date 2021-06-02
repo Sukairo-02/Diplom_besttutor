f937576e2d6cdd57b754a0efbb89769a3d506cb5
Kybbot <kybbot1@gmail.com>
2021-06-02 15:30:59 +0300

    created selectors

---
+++ client/src/redux/selectors.js
@@ -0,0 +1,14 @@
+// subjectSlice
+export const getSubjects = (state) => state.subjects.items;
+
+// taskSlice
+export const getTask = (state) => state.task.item;
+export const getStatistic = (state) => state.task.statistic;
+
+// teachersSlice
+export const getTeachers = (state) => state.teachers.items;
+export const getTeacher = (state) => state.teachers.teacher;
+export const getTeachersFilter = (state) => state.teachers.filter;
+
+// userInfoSlice
+export const getUserInfo = (state) => state.userInfo.info;
