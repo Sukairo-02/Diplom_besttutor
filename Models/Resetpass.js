dede1428e94ef7800ceb22f0a4ca92981a068465
Sukairo-02 <reka.serzh@yandex.ru>
2021-08-04 07:11:48 +0300

    Lint related fixes

--- Models/Resetpass.js
+++ Models/Resetpass.js
@@ -1,9 +1,9 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require('mongoose');
 
 const Resetpass = new Schema({
-    value: { type: String, unique: true, required: true},
-    src: {type: String, required: true},
-    createdAt: { type: Date, expires: "15m", default: Date.now }
-})
+	value: { type: String, unique: true, required: true },
+	src: { type: String, required: true },
+	createdAt: { type: Date, expires: '15m', default: Date.now },
+});
 
-module.exports = model("Resetpass", Resetpass)
+module.exports = model('Resetpass', Resetpass);
